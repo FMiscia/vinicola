3c1cfd72f3c6ffa2f13e92297d32d0306c49fb5a
Francesco Miscia <fra.miscia@gmail.com>
2013-02-21 15:20:42 +0100

    vinicola 1.0

---
+++ includes/config.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+global $config;
+$config["mysql"] = array(
+    "host" => "localhost",
+    "username" => "***",
+    "password" => "*****",
+    "dbname" => "dinzeo");
+$config["smarty"] = array(
+    "template_dir" => "../templates/template1/template/",
+    "compile_dir" => "../templates/template1/templates_c/",
+    "config_dir" => "../templates/template1/configs/",
+    "cache_dir" => "../templates/template1/cache/",
+);
+?>
